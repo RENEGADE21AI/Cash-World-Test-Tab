9ace329cb0e60c0bb0c9c2226710862c2c68416c
RENEGADE21AI <mjdel-bianco@students.wcpss.net>
2025-05-23 07:22:44 -0400

    Update main.js

--- cashWorldTab/cashWorldTabScripts/main.js
+++ cashWorldTab/cashWorldTabScripts/main.js
@@ -1,8 +1,12 @@
 import { updateCamera } from "./camera.js";
 import { drawGrid } from "./gridRenderer.js";
 import { setupInputListeners } from "./input.js";
+import { generateBiomes } from "./biomeGenerator.js";
+import { loadBiomeImages } from "./tileSprites.js";
 
 setupInputListeners();
+loadBiomeImages();
+generateBiomes();
 
 function loop() {
   updateCamera();
