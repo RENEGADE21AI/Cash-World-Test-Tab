50a4bbe4ac297e9ec686da7faa172db7c8bf8ced
RENEGADE21AI <184287164+RENEGADE21AI@users.noreply.github.com>
2025-06-06 08:04:59 -0400

    Create biomeColorMap.js

---
+++ cashWorldTab/cashWorldTabScripts/biomeColorMap.js
@@ -0,0 +1,15 @@
+// biomeColorMap.js
+
+export const biomeColorMap = {
+  "#e4b4a4": "desert", "#e8a890": "desert", "#ecb490": "desert",
+  "#8c9c94": "coniferous_forest", "#94acb4": "coniferous_forest",
+  "#dcdca4": "grassland", "#d0c490": "grassland",
+  "#cc9cb4": "deciduous_forest", "#bc8ca4": "deciduous_forest",
+  "#dcacc4": "deciduous_forest", "#ecdce8": "deciduous_forest",
+  "#a4b49c": "rainforest", "#74847c": "rainforest",
+  "#acacfc": "tundra", "#ececf4": "tundra", "#9c94d4": "tundra",
+  "#8484c4": "tundra", "#b7b7e8": "tundra", "#ccccec": "tundra", "#8cccdc": "tundra",
+  "#acd4a4": "shrubland",
+  "#e0e8e8": "ocean", "#dcdcdc": "ocean", "#bcc4c4": "ocean", "#e4e4e4": "ocean",
+  "#d4d4d4": "ocean", "#ececec": "ocean"
+};
